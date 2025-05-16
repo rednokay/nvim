5dd0195b449de9eb511b1ad6ed36d7d0bdfe739a
rednokay <207755534+rednokay@users.noreply.github.com>
2025-05-16 07:34:41 +0200

    Add tabbing settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,12 @@ vim.o.relativenumber = true
 
 -- Highlight Line
 vim.o.cursorline = true
+
+-- Tab Settings
+vim.o.tabstop = 4		    -- Number of spaces per tab
+vim.o.shiftwidth = 4 		-- Number of spaces for each indentation
+vim.o.expandtab = true 		-- Convert tabs to spaces
+vim.o.smartindent = true	-- Automatically indent new lines
+
+-- Enable Colors
+vim.o.termguicolors = true
