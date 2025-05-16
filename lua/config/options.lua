5d489d05494c29a268d2d1b8617d38a3e804874c
rednokay <207755534+rednokay@users.noreply.github.com>
2025-05-16 11:44:05 +0200

    Add inline diagnostics

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,3 +13,19 @@ vim.o.smartindent = true	-- Automatically indent new lines
 
 -- Enable Colors
 vim.o.termguicolors = true
+
+-- Wrap Lines
+vim.o.wrap = true
+
+-- Inline Diagnostics
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●', -- Icon or character before the message
+    spacing = 6, -- Space between code and message
+    severity = { min = vim.diagnostic.severity.WARN }, -- Show only warnings and errors
+  },
+  signs = true, -- Keep gutter signs
+  underline = true, -- Keep underlines
+  update_in_insert = false, -- Don’t update during typing
+  severity_sort = true, -- Sort by severity
+})
