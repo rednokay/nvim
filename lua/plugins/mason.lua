c2bf0ed11b56bd09650147d8bd8487974062a781
rednokay <207755534+rednokay@users.noreply.github.com>
2025-07-15 12:32:36 +0200

    Add c support

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "mason-org/mason.nvim",
+    "williamboman/mason.nvim",
     cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUpdate" }, -- Lazy-load on commands
     config = function()
       require("mason").setup({
@@ -8,6 +8,17 @@ return {
           border = "rounded", -- Optional: Customize UI
         },
       })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      -- Configure clangd for C/C++ support
+      require("lspconfig").clangd.setup {
+        cmd = { "clangd", "--background-index" }, -- Optional: Enable background indexing
+        filetypes = { "c", "cpp", "objc", "objcpp" }, -- Ensure it works for C files
+      }
     end,
   },
 }
